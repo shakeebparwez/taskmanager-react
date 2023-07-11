178a588c8eed7ee48d11d6a70f0b2efb0c5c95fd
Shakeeb Parwez <shakeebparwez@gmail.com>
2023-07-12 00:02:50 +0530

    Add update button

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -31,7 +31,7 @@ export function AddTask({ tasklist, setTasklist, task, setTask }) {
         value={task.name || ""}
         onChange={e => setTask({...task, name: e.target.value})}
         />
-        <button type="submit">Add</button>
+        <button type="submit">{task.id ? "Update" : "Add"}</button>
       </form>
     </section>
   )
